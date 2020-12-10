bf068a36efc9ad4129224182d0132668dc76137d
Rodrigo Lemos <rodlemoss@gmail.com>
2020-12-10 16:28:43 -0400

    Initial Commit

---
+++ Desafio1/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Desafio1
+//
+//  Created by Rodrigo Lemos on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var downloadButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButton()
+    }
+
+
+    @IBAction func downloadApi(_ sender: Any) {
+        
+    }
+}
+
+
+extension ViewController {
+    
+    func setupButton() {
+        downloadButton.backgroundColor = .clear
+        downloadButton.layer.cornerRadius = 5
+        downloadButton.layer.borderWidth = 2
+        downloadButton.layer.borderColor = UIColor.yellow.cgColor
+        downloadButton.contentEdgeInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+}
+
